e009194345515b128aa57f17e8edc91eff08935f
Pranjal karan <154495267+Pranjal-k22@users.noreply.github.com>
2026-09-01 15:58:22 +0530

    Time: 139 ms (22.73%), Space: 39.9 MB (21.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int low=0, high=0;
+        int low=0;
+        int high=0;
         int res=INT_MIN;
-        unordered_map<char,int> frq;
-        int n=s.size();
-        for(high=0; high<n; high++){
-            frq[s[high]]++;
+        unordered_map<char,int> f;
+        for(high=0; high<s.size(); high++){
+            f[s[high]]++;
             int k=high-low+1;
-            while(frq.size()<k){
-                frq[s[low]]--;
-                if(frq[s[low]]==0){
-                    frq.erase(s[low]);
-
+            while(f.size()<k){
+                f[s[low]]--;
+                if(f[s[low]]==0){
+                    f.erase(s[low]);
                 }
                 low++;
                 k=high-low+1;
             }
             int len=high-low+1;
-            res=max(res,len);
+            res=max(len,res);
         }
-        if(s.empty()){
+        if(res==INT_MIN){
             return 0;
         }
         return res;
